67929f94f1c0ed49d2cf7045f7a34f1c112c6c49
TntBdj <100975876+TntBdj@users.noreply.github.com>
2022-05-04 09:45:38 -0700

    Update ImageManipulator.py

--- ImageManipulator.py
+++ ImageManipulator.py
@@ -1,3 +1,11 @@
 from PIL import Image
-RandomImage = Image.open("6.jpg")
-RandomImage.show()
+UserChoice = 0
+while UserChoice != "100": 
+    while True:
+        UserChoice = int(input("What dragon image do you want (1-10): "))
+        if UserChoice > 0 or UserChoice < 10:
+            break
+        else:
+            print("Invalid Input")
+    RandomImage = Image.open(f"dragon {UserChoice}.jpg")
+    RandomImage.show()
